91a753dc60a6edf0203ebdec7cf791a03259fdb0
Ayush Srivastava <ayush@binaryveda.com>
2023-02-12 03:32:15 +0530

    few edits

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 const ExperienceCard = ({ experience }: Props) => {
   return (
     <article className='flex flex-col self-end rounded-lg items-center space-y-7 flex-shrink-0
-    w-[330px] h-fit md:w-[600px] snap-center bg-[#292929] p-[20px] md:opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+    w-[330px] h-[600px] md:w-[600px] snap-center bg-[#292929] p-[20px] md:opacity-75 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
         <motion.img
             initial={{
                 y: -100,
@@ -29,7 +29,7 @@ const ExperienceCard = ({ experience }: Props) => {
             <p className='font-semibold text-1xl md:text-2xl mt-1'>{experience?.company}</p>
             <div className='flex space-x-2 my-2'>
                 {
-                    experience.technologies.map((technology) => (
+                    experience.technologies && experience.technologies.map((technology) => (
                         <img
                             key={technology._id}
                             className='h-10 w-10 rounded-full'
@@ -39,8 +39,8 @@ const ExperienceCard = ({ experience }: Props) => {
                 }
             </div>
             <p className='uppercase py-5 text-gray-300'>{new Date(experience?.dateStarted).toDateString()} 
-            - {experience.isCurrentWorkingHere ? 'Present' : new Date(experience?.dateEnded).toDateString()}</p>
-            <ul className='list-disc space-y-4 ml-5 text-sm'>
+            - {experience.isCurrentlyWorkingHere ? 'Present' : new Date(experience?.dateEnded).toDateString()}</p>
+            <ul className='list-disc space-y-4 ml-5 pb-5 text-xs md:text-[15px]'>
                 {experience.points.map((point, index) => (
                     <li key={index}>{point}</li>
                 ))}
