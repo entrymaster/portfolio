be23cfd6cc85e490ab3039d2a3936d45ece0bacc
Ayush Srivastava <ayush@binaryveda.com>
2023-02-12 02:01:46 +0530

    Custom Fetching APIs removed

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,10 +1,16 @@
+import { groq } from "next-sanity";
+import { sanityClient } from "../sanity";
 import { Experience } from "../typings";
 
-export const fetchExperience = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`);
+const query = groq`
+    *[_type == "experience"] {
+        ...,
+        technologies[]->
+    }
+`
 
-    const data = await res.json()
-    const experiences: Experience[] = data.experiences;
+export const fetchExperience = async () => {
+    const experiences: Experience[] = await sanityClient.fetch(query)
 
     return experiences;
 }
--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,10 +1,14 @@
+import { sanityClient } from "../sanity";
+import { groq } from 'next-sanity'
 import { PageInfo } from "../typings";
 
-export const fetchPageInfo = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
+const query = groq`
+    *[_type == "pageInfo"][0]
+`
 
-    const data = await res.json()
-    const pageInfo: PageInfo = data.pageInfo;
+export const fetchPageInfo = async () => {
+
+    const pageInfo: PageInfo = await sanityClient.fetch(query)
 
     return pageInfo;
 }
