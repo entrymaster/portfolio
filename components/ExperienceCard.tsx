ecdb7096e5b2af40727cbc3c1d44f5f1bf2f7816
Ayush Srivastava <ayush@binaryveda.com>
2023-02-12 18:19:39 +0530

    tooltip added

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -34,6 +34,7 @@ const ExperienceCard = ({ experience }: Props) => {
                             key={technology._id}
                             className='h-10 w-10 rounded-full'
                             src={urlFor(technology.image).url()}
+                            title={technology.title}
                         />
                     ))
                 }
